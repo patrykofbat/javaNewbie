5a6738b32e1015ded5f170ece2cb8e555823ab86
Patryk Papiór <patrykpapior1997@gmail.com>
2017-10-03 13:46:41 +0200

    com.company first lab of oop

---
+++ src/Largestproductofgrid/Largestproductofgrid.java
@@ -0,0 +1,26 @@
+package Largestproductofgrid;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+
+public class Largestproductofgrid {
+
+    public static void main(String[] args){
+        int[][] grid =new int[20][20];
+        try {
+            Scanner fileIn = new Scanner(new FileReader("C:\\Users\\Patryk\\Desktop\\exampleofgrid.txt"));
+
+            while(fileIn.hasNext()){                 /*looping through file*/
+                if(fileIn.hasNextInt())
+                System.out.println(fileIn.nextInt());
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import java.util.*;
+import java.lang.*;
+
 
 public class Main {
-
     public static void main(String[] args) {
-	System.out.print("Patryk");
+            System.out.print("Podaj pesel: ");
+            Scanner userInput = new Scanner(System.in);
+            Pesel p = new Pesel(userInput.next());
+        if(p.check()){
+            System.out.print("Podana prawidlowy pesel");
+        }
+        else{
+            System.out.print("Zly format pesel");
+        }
+
+
     }
 }
