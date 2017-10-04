b47d93ebfcd31a000a97ff123d06c1ca57bb82c8
Patryk Papiór <patrykpapior1997@gmail.com>
2017-10-04 10:32:49 +0200

    code refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,17 @@
 package com.company;
+
 import java.util.*;
 import java.lang.*;
 
 
 public class Main {
     public static void main(String[] args) {
-            System.out.print("Podaj pesel: ");
-            Scanner userInput = new Scanner(System.in);
-            Pesel p = new Pesel(userInput.next());
-        if(p.check()){
+        System.out.print("Podaj pesel: ");
+        Scanner userInput = new Scanner(System.in);
+        Pesel p = new Pesel(userInput.next());
+        if (p.check()) {
             System.out.print("Podana prawidlowy pesel");
-        }
-        else{
+        } else {
             System.out.print("Zly format pesel");
         }
 
--- src/com/company/Pesel.java
+++ src/com/company/Pesel.java
@@ -2,41 +2,35 @@ package com.company;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.lang.*;
 
 public class Pesel {
     String pesel;
-    Pesel(String p){
+
+    Pesel(String p) {
         pesel = p;
     }
 
-    boolean check(){
+    boolean check() {
         String pattern = "[0-9]+";
         Pattern d = Pattern.compile(pattern);
         Matcher m = d.matcher(pesel);
-        int liczbaKontrolna=0;
-        if(pesel.length()!= 11){
+        int liczbaKontrolna = 0;
+        if (pesel.length() != 11) {
             return false;
-        }
-        else if(!m.matches()){
+        } else if (!m.matches()) {
             return false;
-        }
-
-
-        else
-            for(int i=0;i<pesel.length()-1;i++) {
-                int[]tab={9,7,3,1,9,7,3,1,9,7};
-                Character b = pesel.charAt(i);
-                String g = b.toString();
-                Integer x = Integer.parseInt(g);
-                liczbaKontrolna += x*tab[i];
+        } else
+            for (int i = 0; i < pesel.length() - 1; i++) {
+                int[] tab = {9, 7, 3, 1, 9, 7, 3, 1, 9, 7};
+                int x = Character.getNumericValue(pesel.charAt(i));
+                liczbaKontrolna += x * tab[i];
             }
-            liczbaKontrolna = liczbaKontrolna % 10;
-            Character f = pesel.charAt(10);
-            String h = f.toString();
-            Integer o = Integer.parseInt(h);
-            if(liczbaKontrolna != o)
-                return false;
-            else
-                return true;
+        liczbaKontrolna = liczbaKontrolna % 10;
+        Integer o = Character.getNumericValue(pesel.charAt(10));
+        if (liczbaKontrolna != o)
+            return false;
+        else
+            return true;
     }
 }
