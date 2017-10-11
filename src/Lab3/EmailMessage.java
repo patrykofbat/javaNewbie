056299912ae40b04d66435aebe8b0ac6816b1328
Patryk Papiór <patrykpapior1997@gmail.com>
2017-10-11 19:37:48 +0200

    create send utility

--- src/Lab3/EmailMessage.java
+++ src/Lab3/EmailMessage.java
@@ -2,8 +2,10 @@ package Lab3;
 
 import java.util.LinkedList;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
 
 
 public class EmailMessage {
@@ -14,9 +16,16 @@ public class EmailMessage {
     private String mimeType;
     private LinkedList<String> cc;
     private LinkedList<String> bcc;
+    private static final String SMTP_AUTH_USER ="patrykofbat@gmail.com";
+    private static final String SMTP_AUTH_PWD ="patryk123";
 
     public String getFrom() {
         return from;
+    }
+
+    public void getTo () {
+        for(String to : this.to)
+            System.out.println(to);
     }
 
     private EmailMessage(String from_, LinkedList<String> to_, String subject_, String content_, String mimeType_, LinkedList<String> cc_,
@@ -35,8 +44,42 @@ public class EmailMessage {
         return new EmailMessage.Builder();
     }
 
-    public void send() {
-        Properties props = new Properties();
+    public void send()throws MessagingException {
+        Properties props = System.getProperties();
+        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.user", SMTP_AUTH_USER);
+        props.setProperty("mail.smtp.password", SMTP_AUTH_PWD);
+        props.setProperty("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(SMTP_AUTH_USER, SMTP_AUTH_PWD);
+            }
+        });
+        try{
+            MimeMessage message = new MimeMessage(session);
+
+
+            message.setFrom(new InternetAddress(this.from));
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(this.to.get(0)));
+
+            message.setSubject(this.subject);
+
+            message.setText(this.content);
+
+            Transport.send(message);
+
+            System.out.println("Message sent successfully");
+        }catch(MessagingException mex){
+            mex.printStackTrace();
+        }
+
+
 
 
     }
--- src/Lab3/Main.java
+++ src/Lab3/Main.java
@@ -1,14 +1,19 @@
 package Lab3;
 
 
+import javax.mail.MessagingException;
+
 public class Main {
     public static void main(String[] args){
         try {
-            EmailMessage wiadomosc = EmailMessage.builder().addFrom("sspl@pl").addTo("Mail@wp", "hehwp").build();
+            EmailMessage wiadomosc = EmailMessage.builder().addFrom("patrykpapior1997@gmail.com").addTo("patrykpapior1997@gmail.com").
+                    addSubject("Siemka").addContent("siemka").build();
             System.out.println(wiadomosc.getFrom());
+            wiadomosc.getTo();
+            wiadomosc.send();
 
         }
-        catch(NullPointerException|InvalidEmailFormatException|RequriedArgumentsException c) {
+        catch(MessagingException|NullPointerException|InvalidEmailFormatException|RequriedArgumentsException c) {
 
 
         }
