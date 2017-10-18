7c237443d102fe9bd5ac1dd7c6db39d9ab1edb58
Patryk Papiór <patrykpapior1997@gmail.com>
2017-10-18 13:36:39 +0200

    lab 4

--- src/Lab4/MainLab4.java
+++ src/Lab4/MainLab4.java
@@ -1,17 +1,41 @@
 package Lab4;
 
 import java.io.File;
+import java.util.Scanner;
+
+// to set up program args go to Run -> Edit configurations
 
 public class MainLab4 {
     public static void main(String[] args){
-        File inputFile = new File("C:\\Users\\Patryk\\Documents\\FileJava\\input.txt");
+        File in = new File(args[0]);
+        File out = new File(args[1]);
+        System.out.println("1.szyfruj(ROT11)");
+        System.out.println("2.szyfruj(Polibiusz)");
+        System.out.println("3.deszyfruj(ROT11)");
+        System.out.println("4.deszyfruj(Polibius)");
+        System.out.print("Wybor: ");
+        Scanner userInput = new Scanner(System.in);
+        int i = userInput.nextInt();
+        switch (i) {
+            case 1:
+                Cryptographer.cryptFile(in, out, new ROT11());
+                break;
+            case 2:
+                Cryptographer.cryptFile(in, out, new Polibius());
+                break;
+            case 3:
+                Cryptographer.deCryptFile(in, out, new ROT11());
+                break;
+            case 4:
+                Cryptographer.deCryptFile(in, out, new Polibius());
+                break;
+        }
+    }
+       /* File inputFile = new File("C:\\Users\\Patryk\\Documents\\FileJava\\input.txt");
         File outputFile = new File("C:\\Users\\Patryk\\Documents\\FileJava\\output.txt");
         File newFile = new File("C:\\Users\\Patryk\\Documents\\FileJava\\decrypted.txt");
-        Cryptographer.cryptFile(inputFile, outputFile, new ROT11());
-        Cryptographer.deCryptFile(outputFile, newFile, new ROT11());
-
-
-
-
-    }
+        Cryptographer.cryptFile(inputFile, outputFile, new Polibius());
+        Cryptographer.deCryptFile(outputFile, newFile, new Polibius());
+        */
 }
+
--- src/Lab4/Polibius.java
+++ src/Lab4/Polibius.java
@@ -4,20 +4,56 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Polibius implements Algorithm {
-    private final static char[][] POLYBIUSARRAYCODER= {{'a','b','c','d','e'},{'f','g','h','i','k'},{'l','m','n','o','p'}
-    ,{'q','r','s','t','u'},{'v','w','x','y','z'}};
+    private final static char[][] POLYBIUSARRAYCODER = {{'a', 'b', 'c', 'd', 'e'}, {'f', 'g', 'h', 'i', 'k'}, {'l', 'm', 'n', 'o', 'p'}
+            , {'q', 'r', 's', 't', 'u'}, {'v', 'w', 'x', 'y', 'z'}};
+
     @Override
     public String crypt(String word) {
-        char[] cryptedMessage = new char[word.length()*2];
-        for(int i=0;i<word.length();i++){
+        String cryptedMessage = "";
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            int licznik = 0;
+            while (licznik < 5) {
+                for (char k : POLYBIUSARRAYCODER[licznik]) {
+                    if (k == word.charAt(i)) {
+                        int tmpInt = (licznik + 1) * 10 + Arrays.binarySearch(POLYBIUSARRAYCODER[licznik], word.charAt(i)) + 1;
+                        cryptedMessage = cryptedMessage + Integer.toString(tmpInt);
+                        break;
+                    } else if (word.charAt(i) == 'j') {
+                        cryptedMessage = cryptedMessage + "24";
+                        break;
+                    } else if (word.charAt(i) == ' ') {
+                        cryptedMessage = cryptedMessage + " ";
+                        break;
+                    }
+
+
+                }
+                licznik++;
+
+            }
 
         }
 
-        return null;
+        return cryptedMessage;
     }
 
     @Override
     public String decrypt(String cryptedWord) {
-        return null;
+        String decrypted = "";
+        int i =0;
+        while(i<=cryptedWord.length()-1){
+            if(cryptedWord.charAt(i) == ' '){
+                decrypted = decrypted + " ";
+                i++;
+            }
+            else {
+                int row = Character.getNumericValue(cryptedWord.charAt(i)) - 1;
+                int col = Character.getNumericValue(cryptedWord.charAt(i + 1)) - 1;
+                decrypted = decrypted + POLYBIUSARRAYCODER[row][col];
+                i+=2;
+            }
+        }
+        return decrypted;
     }
 }
